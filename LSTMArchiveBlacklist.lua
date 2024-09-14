3781e408290e637babb9431b00fc136a54e5178b
AURORA666Script <auroralittle@qq.com>
2024-09-14 22:07:36 +0800

    Add files via upload

---
+++ LSTMArchiveBlacklist.lua
@@ -0,0 +1,6 @@
+return {
+["123fa98"] = true,
+["Krx"] = true,
+["CloudScripts"] = true,
+["wuai005"] = true,
+}
---
+++ LSTMArchiveWhitelist.lua
@@ -0,0 +1,6 @@
+return {
+["wevyyds"] = true,--bai
+["hyboy"] = true,--Friends
+["baijin"] = true,--臭白金
+["ditto_idk"] = true,--Qmuuuuuuuuuuuuu
+}
