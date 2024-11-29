304802e2fe0c35f5a389e85602b25bd4d8c3fbd1
AURORA666Script <auroralittle@qq.com>
2024-11-29 18:28:21 +0800

    Add files via upload

---
+++ state.js
@@ -0,0 +1,15 @@
+{
+  "name": "LSTM Archive",
+  "Webhook": "No work",
+  "state": "Working",
+  "Script status": [
+    "Muscle Legend", true,
+    "Ninja Legend", true,
+    "Legends of Speed", true,
+    "General", true
+  ],
+  "UI Setting": {
+    "Color": "Red",
+    "languages": ["Chinese", "English"]
+  }
+}
